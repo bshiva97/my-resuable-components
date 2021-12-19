5340cbfc7127f0089751124e7ba27f5b525435f7
Shivam Bansal <archisanjeev@outlook.com>
2021-12-19 23:39:58 +0530

    commit for sample reusbale component

---
+++ src/SimpleButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const SimpleButton = () =>{
+    return(
+        <React.Fragment>
+        <button style={{backgroundColor:"red"}}>Hey Button</button>
+        </React.Fragment>
+    )
+}
+
+export default SimpleButton;
